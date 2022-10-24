4192815957ae8270dae15faf356eb37eac20c4d7
Mr Loll <walidelloll2018@gmail.com>
2022-10-24 14:09:18 +0200

    edit the upper bar and add language toggle button

---
+++ js/edit.js
@@ -0,0 +1,9 @@
+(function ($) {
+    "use strict";
+
+    $(".upper-bar .change_lang").on("click", () => {
+        $(".upper-bar .dropdown-list .wrapper").slideToggle(200)
+    })
+    
+})(jQuery);
+
