8850f898aff4b928565f2fc16c4f465c1a2f2698
Mr Loll <walidelloll2018@gmail.com>
2022-10-24 18:17:09 +0200

    adding new photos and editour services and adding overlay with details on click

--- js/edit.js
+++ js/edit.js
@@ -5,5 +5,16 @@
         $(".upper-bar .dropdown-list .wrapper").slideToggle(200)
     })
     
+        // show details button
+    $(".services .show-details").on("click", function (e) {
+        e.preventDefault();
+        $(this).parents(".service-item").find(".details-overlay").show(500)
+    });
+
+    $(".services .service-item .details-overlay .fa-times").on("click", function (e) {
+        e.preventDefault();
+        $(this).parents(".details-overlay").hide(500);
+    });
+
 })(jQuery);
 
