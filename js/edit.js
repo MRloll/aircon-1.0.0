d062a0d2608badd1091aafed9d5aa052fc7cff6e
Mr Loll <walidelloll2018@gmail.com>
2022-10-28 18:02:17 +0200

    changin service over lay & edits

--- js/edit.js
+++ js/edit.js
@@ -5,16 +5,7 @@
         $(".upper-bar .dropdown-list .wrapper, nav .dropdown-list .wrapper").slideToggle(200)
     })
     
-        // show details button
-    $(".services .show-details").on("click", function (e) {
-        e.preventDefault();
-        $(this).parents(".service-item").find(".details-overlay").show(500)
-    });
 
-    $(".services .service-item .details-overlay .fa-times").on("click", function (e) {
-        e.preventDefault();
-        $(this).parents(".details-overlay").hide(500);
-    });
 
 })(jQuery);
 
